0a934cf56f4e1c4950f25f675233fe1f6c99bee2
Nikita K <vrolok15@yandex.ru>
2017-05-21 02:29:48 +0300

    Defining Vertex3d class

--- CSharp_Excercises/Vertex3d.cs
+++ CSharp_Excercises/Vertex3d.cs
@@ -5,7 +5,33 @@ using System.Text;
 
 namespace CSharp_Excercises
 {
-    class Vertex3d
+    public class Vertex3d
     {
+        private double _x;
+        private double _y;
+        private double _z;
+
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public double Z
+        {
+            get { return _z; }
+            set { _z = value; }
+        }
+
+        public void SetToOrigin()
+        {
+            X = Y = Z = 0.0;
+        }
     }
 }

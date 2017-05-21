98a07663c083511dc681da0bf8626070f08e1916
Nikita K <vrolok15@yandex.ru>
2017-05-21 12:29:09 +0300

    Added Base and Derived class

---
+++ CSharp_Excercises/DerivedClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Excercises
+{
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass()
+        {
+            // _a = 1; It's set to private in Base Class, thus not accessible
+            _b = 2;
+            _c = 3;
+        }
+
+        public void DoSomething()
+        {
+            _b = _c = 100;
+        }
+    }
+}
